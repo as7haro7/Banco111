bef947f951ae6dcc58ec325a05e26db39866eceb
as7haro7 <erickbreack72@gmail.com>
2023-05-26 03:17:57 -0400

    La clase Historial ya tiene las funciones basicas

--- historial.py
+++ historial.py
@@ -1,25 +1,24 @@
 import json
+import datetime
+
 class Historial:
-    def __init__(self, codigo_cuenta, tipo_transaccion, fecha):
+    def __init__(self):
         self.transacciones = []
-        self.__codigo_cuenta = codigo_cuenta
-        self.__tipo_transaccion = tipo_transaccion
-        self.__fecha = fecha
+        self.__codigo_cuenta = None
+        self.__tipo_transaccion = None
+        self.__fecha = None
 
-    def __str__(self):
-        return f'codigo cuenta: {self.__codigo_cuenta}, tipo de transaccion: {self.__tipo_transaccion}, fecha: {self.__fecha}'
-
-    # getters
+    # Geters
     def get_codigo_cuenta(self):
         return self.__codigo_cuenta
-
+    
     def get_tipo_transaccion(self):
         return self.__tipo_transaccion
-    
+
     def get_fecha(self):
         return self.__fecha
     
-    # setters    
+    # Seters
     def set_codigo_cuenta(self, codigo_cuenta):
         self.__codigo_cuenta = codigo_cuenta
 
@@ -29,77 +28,103 @@ class Historial:
     def set_fecha(self, fecha):
         self.__fecha = fecha
 
-
-
-# metodos         
-    # @staticmethod
-    # def ListarHistorial(diccionario_historial, codigo):
-    #     for codigo, historial in diccionario_historial.items():
-    #         if codigo == codigo_cuenta:
-    #             return(historial)
-            
+    # Metodo str
+    def __str__(self):
+        return f"Código de cuenta: {self.__codigo_cuenta}, Tipo de transacción: {self.__tipo_transaccion}, Fecha: {self.__fecha}"
+    
+    # Metodos de la Clase        
+    def agregar_transaccion(self):
+        transaccion = {
+            'codigo_cuenta': self.__codigo_cuenta,
+            'tipo_transaccion': self.__tipo_transaccion,
+            'fecha': self.__fecha
+        }
+        self.transacciones.append(transaccion)
 
     
-    def agregar_transaccion(self, codigo_cuenta, tipo_transaccion, fecha):
-        transaccion = {
-            'tipo_transaccion': tipo_transaccion,
-            'fecha': fecha
-        }
-        if codigo_cuenta in self.transacciones:
-            self.transacciones[codigo_cuenta].append(transaccion)
-        else:
-            self.transacciones[codigo_cuenta] = [transaccion]
+    def guardar_transacciones_json(self):
+        try:
+            with open("transacciones.json", 'r') as f:
+                datos_previos = json.load(f)
+        except FileNotFoundError:
+            datos_previos = []
+        
+        datos_actuales = datos_previos + self.transacciones
+        
+        with open("transacciones.json", 'w') as f:
+            json.dump(datos_actuales, f, indent=4)
 
-    def guardar_transacciones(self, archivo):
-        with open(archivo, 'w') as file:
-            json.dump(self.transacciones, file)
+    def listar_transacciones_por_cuenta(self, codigo_cuenta):
+        transacciones_cuenta = []
+        with open("transacciones.json", 'r') as f:
+            data = json.load(f)
+            for transaccion in data:
+                if transaccion['codigo_cuenta'] == codigo_cuenta:
+                    transacciones_cuenta.append(transaccion)
+        return transacciones_cuenta
+
+
 
 if __name__ == '__main__':
-    historial = Historial()
-    historial.agregar_transaccion(123, 'Depósito', '2023-05-25')
-    historial.agregar_transaccion(123, 'Retiro', '2023-05-26')
-    historial.guardar_transacciones('transacciones.json')
+    fecha_actual = datetime.datetime.now()   
+    fecha_actual_formateada = fecha_actual.strftime('%Y-%m-%d %H:%M:%S')
+
+# #-------------------Pruebas de los getter y setter------------------
+#     historial = Historial()
+#     historial.set_codigo_cuenta(1)
+#     historial.set_tipo_transaccion('Deposito')
+#     historial.set_fecha('2023-05-25')
+
+#     codigo_inicial = historial.get_codigo_cuenta()
+#     tipo_inicial = historial.get_tipo_transaccion()
+#     fecha_inicial = historial.get_fecha()
+
+#     print(codigo_inicial)  
+#     print(tipo_inicial)  
+#     print(fecha_inicial)  
+    
+# # modificacion de datos
+#     historial = Historial()
+#     historial.set_codigo_cuenta(1)
+#     historial.set_tipo_transaccion('Retiro')
+#     historial.set_fecha(fecha_actual_formateada)
+
+#     codigo_modificado = historial.get_codigo_cuenta()
+#     tipo_modificado = historial.get_tipo_transaccion()
+#     fecha_modificada = historial.get_fecha()
+
+#     print(codigo_modificado)  
+#     print(tipo_modificado)  
+#     print(fecha_modificada)   
+
+# # --------------------------esta parte es para probar el metodo de listado por cuenta-------------------
+#     historial = Historial()
+#     transacciones_cuenta = historial.listar_transacciones_por_cuenta(67890)
+#     for transaccion in transacciones_cuenta:
+#         print(transaccion)
+
+    
+    
+# # # --------------------------esta parte es para probar el El guardado de instacian en JSON-------------------
+#     historial = Historial()
+
+#     fecha_actual = datetime.datetime.now()   
+#     fecha_actual_formateada = fecha_actual.strftime('%Y-%m-%d %H:%M:%S')
+
+#     historial.set_codigo_cuenta(6)
+#     historial.set_tipo_transaccion('Deposito')
+#     historial.set_fecha(fecha_actual_formateada)
+#     historial.agregar_transaccion()
+#     historial.guardar_transacciones_json()
 
 
 
 
-
-# historial = Historial(codigo_cuenta=1, tipo_transaccion="Depósito", fecha="01/01/2023")
-# print(historial)
-
-# # Usar los métodos getter para obtener los valores de los atributos
-# codigo_cuenta = historial.get_codigo_cuenta()
-# tipo_transaccion = historial.get_tipo_transaccion()
-# fecha = historial.get_fecha()
-# print(codigo_cuenta, tipo_transaccion, fecha)
-
-# # Usar los métodos setter para actualizar los valores de los atributos
-# historial.set_codigo_cuenta(2)
-# historial.set_tipo_transaccion("Retiro")
-# historial.set_fecha("02/01/2023")
-# print(historial.get_codigo_cuenta(), historial.get_tipo_transaccion(), historial.get_fecha())
+# # ----------probar el metodo __str__-----------------
+#     historial = Historial()
+#     historial.set_codigo_cuenta(12345)
+#     historial.set_tipo_transaccion('Deposito')
+#     historial.set_fecha(fecha_actual_formateada)
+#     print(historial)
 
 
-
-
-# # Prueba de que estan funcionando los metodos
-# historial = {} 
-
-# historial1 = Historial(codigo_cuenta=1, tipo_transaccion="Deposito", fecha="01/01/2023")
-# historial[historial1._codigo_cuenta] = historial1
-
-# historial2 = Historial(codigo_cuenta=2, tipo_transaccion="Retiro", fecha="02/01/2023")
-# historial[historial2._codigo_cuenta] = historial2
-
-# historial3 = Historial(codigo_cuenta=3, tipo_transaccion="Transferencia", fecha="03/01/2023")
-# historial[historial3._codigo_cuenta] = historial3
-
-
-# # Guardar el diccionario en un archivo JSON con la función de serialización personalizada
-# with open("historial.json", "w") as archivo_json:
-#     json.dump(historial, archivo_json, default=Historial.serializar_historial)
-
-
-# codigo_prueba = 1
-# cuenta_x=Historial.ListarHistorial(historial,codigo_prueba)
-# print(cuenta_x)
